fd3d7b9015b9e1e12bdc69379d2ac033f1a96876
Cheng Long (Centific Technologies Inc) <v-chenglong@microsoft.com>
2024-12-12 15:32:27 -0800

    update repo

---
+++ test1_start.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+
+    return (
+        <button onClick={onClick}>
+            Click me
+        </button>
+    )
---
+++ test2_start.py
@@ -0,0 +1,5 @@
+def debug(arr1, arr2, arr3):
+    for i in range(len(arr1)):
+        print(arr1[i])
+        print(arr2[i])
+        print(arr3[i])
